c0e5dff5599dfa62b1aab73147e10e947f20c814
amat <amat@mail.com>
2025-05-30 20:11:59 +0700

    remove: bg-chatBot, color in covert page. responsive: chatbot-card

--- src/app/dashboard/consultation/page.tsx
+++ src/app/dashboard/consultation/page.tsx
@@ -6,8 +6,6 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 import { Loader, SendHorizontal } from 'lucide-react';
 import React from 'react';
 import { toast } from 'sonner';
-import BackgroundImage from './backgroundMassage';
-import { TextAnimate } from '@/components/magicui/text-animate';
 
 export default function ConsultationAi() {
   const [message, setMessage] = React.useState('');
@@ -45,60 +43,50 @@ export default function ConsultationAi() {
   };
 
   return (
-    <div className="flex justify-center items-center ">
-      <Card className="w-full max-w-[800px] shadow-none border-none fixed bottom-5 px-2">
-        <BackgroundImage />
+    <div className="flex justify-center items-center px-2">
+      <Card className="w-full max-w-[800px] h-[84vh] shadow-none border-none flex flex-col justify-between bg-transparent">
         <CardHeader>
           <CardTitle className="text-center">SpendeeBot</CardTitle>
         </CardHeader>
 
-        <CardContent className="space-y-4 overflow-y-auto ">
-          <ScrollArea className="h-170 rounded-md max-h-[580px] ">
-            {chats.map((chat, index) => (
-              <div
-                key={index}
-                className={`max-w-[60%] w-fit p-2 px-5 mt-2 shadow-sm ${
-                  chat.sender === 'You' ? 'ml-auto bg-muted-foreground text-secondary rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl text-right' : 'mr-auto bg-sidebar-accent rounded-tl-2xl rounded-tr-2xl rounded-br-2xl text-left'
-                }`}
-              >
-                {/* <span className="text-sm font-medium">{chat.sender === 'You' ? '' : 'SpendeeBot:'}</span> */}
-                <p className="text-sm font-light mt-1 whitespace-pre-wrap">{chat.message}</p>
-              </div>
-            ))}
+        <CardContent className="flex-1 overflow-hidden px-4">
+          <ScrollArea className="h-full">
+            <div className="flex flex-col space-y-4">
+              {chats.map((chat, index) => (
+                <div
+                  key={index}
+                  className={`max-w-[60%] w-fit p-2 px-5 mt-2 shadow-sm ${
+                    chat.sender === 'You' ? 'ml-auto bg-muted-foreground text-secondary rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl text-right' : 'mr-auto bg-sidebar-accent rounded-tl-2xl rounded-tr-2xl rounded-br-2xl text-left'
+                  }`}
+                >
+                  <p className="text-sm font-light mt-1 whitespace-pre-wrap">{chat.message}</p>
+                </div>
+              ))}
 
-            {/* Bubble “Mengetik...” ketika sedang loading */}
-            {loading && (
-              <div className="max-w-[20%] mr-auto bg-sidebar-accent text-left p-3 rounded-tl-2xl rounded-tr-2xl rounded-br-2xl">
-                {/* <span className="text-sm font-medium">SpendeeBot:</span> */}
-                <p className="text-sm mt-1 italic">
-                  <TextAnimate
-                    animation="fadeIn"
-                    by="line"
-                    as="p"
-                  >
-                    Wait...
-                  </TextAnimate>
-                </p>
-              </div>
-            )}
+              {loading && (
+                <div className="max-w-[20%] mr-auto bg-sidebar-accent text-left p-3 rounded-tl-2xl rounded-tr-2xl rounded-br-2xl">
+                  <p className="text-sm mt-1 italic">
+                      Wait...
+                  </p>
+                </div>
+              )}
+            </div>
           </ScrollArea>
         </CardContent>
 
-        {/* <CardFooter className="p-4 ">
-          </CardFooter> */}
         <form
           onSubmit={handleSubmit}
-          className="w-full gap-2 flex justify-center"
+          className="w-full gap-2 flex px-4 "
         >
           <Input
             placeholder="Tanya SpendeeBot..."
-            className="p-5 rounded-4xl border-2"
+            className="flex-1 p-4 rounded-3xl border"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
           />
           <Button
             type="submit"
-            className="rounded-3xl p-5 "
+            className="rounded-full p-4"
             disabled={loading}
           >
             {loading ? <Loader /> : <SendHorizontal />}
