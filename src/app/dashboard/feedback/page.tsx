3c982020b54a53753aaed7696c8a9176a8515497
amat <amat@mail.com>
2025-06-02 19:38:19 +0700

    fixed: feedbackPage

--- src/app/dashboard/feedback/page.tsx
+++ src/app/dashboard/feedback/page.tsx
@@ -1,32 +1,9 @@
-import FeedbackPage from "@/components/form-feedback";
+import FeedbackPage from '@/components/form-feedback';
 
 export default function FeedbackForm() {
   return (
-    <div className='flex justify-around h-full'>
-        <Card className="max-w-[650px] w-full ">
-      <CardHeader>
-        <CardTitle>Any feedback for Spendee?</CardTitle>
-        <CardDescription>We are very open to your feedback.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Email</Label>
-              <Input id="email" placeholder="Enter your email" className='w-full' />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Your feedback</Label>
-              <Textarea className='w-full' id="feedback" placeholder="Enter your feedback" />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter >
-        <Button type='submit'>Send Feedback</Button>
-      </CardFooter>
-    </Card>
+    <div>
+      <FeedbackPage></FeedbackPage>
     </div>
-    <FeedbackPage></FeedbackPage>
-  )
+  );
 }
