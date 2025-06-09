00b4fa03f1a562d951b2acc1ce519621b720f633
amat <amat@mail.com>
2025-06-09 21:34:13 +0700

    fixed: history response api

--- src/app/dashboard/eligibility/personality/dialog.tsx
+++ src/app/dashboard/eligibility/personality/dialog.tsx
@@ -33,7 +33,7 @@ export default function AlertDialogPersonality(props: PropsResponse) {
             };
 
             try {
-                const res = await fetch('http://13.54.145.211:3000/api/prediction/simpan', {
+                const res = await fetch(`${process.env.NEXT_PUBLIC_API_BACKEND}/api/prediction/simpan`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
--- src/components/dialog.tsx
+++ src/components/dialog.tsx
@@ -24,7 +24,7 @@ export default function AlertDialogResponse(props: PropsResponse) {
       };
 
       try {
-        const res = await fetch('http://13.54.145.211:3000/api/prediction/simpan', {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_BACKEND}/api/prediction/simpan`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
